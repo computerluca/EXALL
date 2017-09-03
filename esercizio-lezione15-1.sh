42b5d7ac0d072de6ce9fb77dc3a9fb655a6eb451
Luca Traversari <computerlucaworld@gmail.com>
2017-09-03 17:04:23 +0200

    Update esercizio-lezione15-1.sh

--- esercizio-lezione15-1.sh
+++ esercizio-lezione15-1.sh
@@ -1,2 +1,12 @@
 
 #!/bin/bash
+#Ciclare su tutti i file con estensione php di una cartella  e creare una nuova cartella che contiene tali file.
+#Eseguire la stessa operazione sia con un ciclo for sia con un solo comando, che abbiamo visto nelle lezioni precedenti
+mkdir cartella
+for file in *.php; 
+do
+   cp "$file" "cartella/$file"
+done
+#con un solo comando
+
+cp *.php cartella 
